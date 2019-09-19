20ef6a4728b96290793cfd47a4ff4697a8e14760
ThanhSonITNIC <ThanhSon.ITNIC@gmail.com>
2019-09-19 10:10:01 +0700

    add cleardb connection

--- config/database.php
+++ config/database.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Str;
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
 
     /*
@@ -91,6 +98,16 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'cleardb' => array(
+            'driver'    => 'mysql',
+            'host'      => $host,
+            'database'  => $database,
+            'username'  => $username,
+            'password'  => $password,
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ),
     ],
 
     /*
