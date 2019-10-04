4ddb920928c4e80b8aa254d382c900d0c3066bee
ThanhSonITNIC <ThanhSon.ITNIC@gmail.com>
2019-10-04 20:48:26 +0700

    cleardb to mysql

--- config/database.php
+++ config/database.php
@@ -2,12 +2,15 @@
 
 use Illuminate\Support\Str;
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+/**
+ * CLEARDB
+ */
+// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
+// $host = $url["host"];
+// $username = $url["user"];
+// $password = $url["pass"];
+// $database = substr($url["path"], 1);
 
 return [
 
@@ -98,16 +101,16 @@ return [
             'prefix_indexes' => true,
         ],
 
-        'cleardb' => array(
-            'driver'    => 'mysql',
-            'host'      => $host,
-            'database'  => $database,
-            'username'  => $username,
-            'password'  => $password,
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
-        ),
+        // 'cleardb' => array(
+        //     'driver'    => 'mysql',
+        //     'host'      => $host,
+        //     'database'  => $database,
+        //     'username'  => $username,
+        //     'password'  => $password,
+        //     'charset'   => 'utf8',
+        //     'collation' => 'utf8_unicode_ci',
+        //     'prefix'    => '',
+        // ),
     ],
 
     /*
